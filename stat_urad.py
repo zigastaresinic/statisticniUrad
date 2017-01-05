f711c878785a9645a083f98a51c81947a6eebc17
Žiga Starešinič <ziga.staresinic@student.fmf.uni-lj.si>
2017-01-05 11:01:29 +0100

    delal na modelih

--- stat_urad.py
+++ stat_urad.py
@@ -5,9 +5,24 @@ import modeli
 def index():
     return template('zacetna_stran')
 
+@post('/')
+def prijavare():
+    upIme = request.forms.upIme
+    geslo = request.forms.geslo
+    aliPravo = modeli.prijava(upIme,geslo)
+    #print(upIme,geslo,aliPravo)
+    if aliPravo is not None:
+        (sektor,stSek) = modeli.poisciSektor(upIme) ##vrnemo sektor in st vseh sektorjev za zanko,
+                                            ##ce se v prihodnosti odločimo, da dodamo še kak sektor
+        for i in range(1,stSek+1):
+            if sektor == 4:
+                return redirect('/admin/')
+    else:
+        return redirect('/pomoc/')
+
 @route('/pomoc/')
 def pomoc():
-    return 'Pojdi na stran /delitelji/xxx/ za delitelje stevila xxx.'
+    return 'Narobe ste vnesli geslo.'
 
 @route('/dodaj_uporabnika/')
 def dodaj_uporabnika():
@@ -22,15 +37,13 @@ def dodaj_uporabnika():
         modeli.dodaj_uporabnika(up,pas,sek)
             
     return redirect('/dodaj_uporabnika/')
-
-##@route('/prijava/')
-##def prijava():
-##    upIme = request.forms.uporabnik
-##    geslo = request.forms.uporabnik
-##    aliPravo = modeli.prijava(upIme,geslo)
-##    if aliPravo == None:
-##        return redirect('/prijava/')
-##    else:
-##        return redirect('/pomoc/')
     
+@route('/prijava/')
+def prijava():
+    return template('prijava')
+
+@route('/admin/')
+def admin():
+    return template('admin')
+#https://bottlepy.org/docs/dev/tutorial.html#id3
 run(debug = True)
